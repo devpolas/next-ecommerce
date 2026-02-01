ce00fb521b5a123afc5e950d4320f43535f047e8
devpolas <195308698+devpolas@users.noreply.github.com>
2026-02-01 18:26:31 +0600

    implement themming with next theme

--- components/shared/header/theme-toggler.tsx
+++ components/shared/header/theme-toggler.tsx
@@ -1,3 +1,64 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useLayoutEffect, useState } from "react";
+
 export default function ThemeToggler() {
-  return <div> Toggle</div>;
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme, resolvedTheme } = useTheme();
+
+  const currentTheme = theme === "system" ? resolvedTheme : theme;
+
+  useLayoutEffect(() => {
+    // eslint-disable-next-line
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          aria-label='Toggle theme'
+          variant='ghost'
+          className='focus-visible:ring-0 focus-visible:ring-offset-0'
+        >
+          {currentTheme === "dark" ? <MoonIcon /> : <SunIcon />}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem
+          onSelect={() => setTheme("system")}
+          checked={theme === "system"}
+        >
+          System
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          onSelect={() => setTheme("dark")}
+          checked={theme === "dark"}
+        >
+          Dark
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          onSelect={() => setTheme("light")}
+          checked={theme === "light"}
+        >
+          Light
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
