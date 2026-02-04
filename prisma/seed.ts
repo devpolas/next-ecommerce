41519ccc86a29dbd09ed99ad6e892ab45c128fa4
devpolas <195308698+devpolas@users.noreply.github.com>
2026-02-05 00:08:49 +0600

    fix bug - 1

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,8 @@
 import { PrismaClient, Prisma } from "@/lib/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaNeon } from "@prisma/adapter-neon";
 import "dotenv/config";
 
-const adapter = new PrismaPg({
+const adapter = new PrismaNeon({
   connectionString: process.env.DATABASE_URL,
 });
 
